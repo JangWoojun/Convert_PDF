c502eccfbcc36d9ffaf6d939dfe602d95fe7f1d3
JangWoojun <woojun0107@naver.com>
2024-01-17 10:14:58 +0900

    fix: compileSdk 33 오류 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.woojun.convertpdf"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.woojun.convertpdf"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
